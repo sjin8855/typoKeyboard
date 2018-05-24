0b52a3cbde0c537bb9b5abb62cc0c1b8357464ae
Sangjin-Lee <mytkdwlsaos@daum.net>
2018-05-24 17:06:11 +0900

    Add keyboard shift and caps and convert

--- app/src/main/java/com/example/sangjin_lee/typokeyboard/typoKeyboard.java
+++ app/src/main/java/com/example/sangjin_lee/typokeyboard/typoKeyboard.java
@@ -13,8 +13,9 @@ public class typoKeyboard extends InputMethodService
 
     private KeyboardView kv;
     private Keyboard keyboard;
-
+    private boolean shift = false;
     private boolean caps = false;
+    private String kor = "1";
 
     @Override
     public View onCreateInputView() {
@@ -27,6 +28,8 @@ public class typoKeyboard extends InputMethodService
 
     private void playClick (int keyCode) {
         AudioManager am = (AudioManager) getSystemService(AUDIO_SERVICE);
+
+
         switch (keyCode) {
             case 32:
                 am.playSoundEffect(AudioManager.FX_KEYPRESS_SPACEBAR);
@@ -43,6 +46,7 @@ public class typoKeyboard extends InputMethodService
         }
     }
 
+
     @Override
     public void onPress(int primaryCode) {
 
@@ -58,24 +62,51 @@ public class typoKeyboard extends InputMethodService
         InputConnection ic = getCurrentInputConnection();
         playClick(primaryCode);
 
+
+
         switch(primaryCode){
             case Keyboard.KEYCODE_DELETE:
                 ic.deleteSurroundingText(1,0);
                 break;
             case Keyboard.KEYCODE_SHIFT:
-                caps = !caps;
-                keyboard.setShifted(caps);
+                shift = !shift;
+                keyboard.setShifted(shift^caps);
                 kv.invalidateAllKeys();
                 break;
             case Keyboard.KEYCODE_DONE:
                 ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
                 break;
+            case -3:
+                caps = !caps;
+                keyboard.setShifted(caps);
+                kv.invalidateAllKeys();
+                break;
+            case Keyboard.KEYCODE_MODE_CHANGE:
+                if(kor.equals("1")){
+                    kor = "2";
+                    keyboard = new Keyboard(this, R.xml.korean);
+                    kv.setKeyboard(keyboard);
+                    kv.setOnKeyboardActionListener(this);
+                }
+                else {
+                    kor = "1";
+                    keyboard = new Keyboard(this, R.xml.qwerty);
+                    kv.setKeyboard(keyboard);
+                    kv.setOnKeyboardActionListener(this);
+                }
+                break;
             default:
                 char code = (char) primaryCode;
-                if(Character.isLetter(code) && caps) {
+                if(Character.isLetter(code) && (caps^shift)) {
                     code = Character.toUpperCase(code);
                 }
                 ic.commitText(String.valueOf(code), 1);
+                if(shift){
+                    shift = !shift;
+                    keyboard.setShifted(caps^shift);
+                    kv.invalidateAllKeys();
+                }
+
         }
     }
 

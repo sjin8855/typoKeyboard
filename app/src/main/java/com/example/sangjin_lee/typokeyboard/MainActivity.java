e3445b5ad43df8aabe667f5156f3c38fc737b21e
Sangjin-Lee <mytkdwlsaos@daum.net>
2018-05-25 22:05:37 +0900

    720px

--- app/src/main/java/com/example/sangjin_lee/typokeyboard/MainActivity.java
+++ app/src/main/java/com/example/sangjin_lee/typokeyboard/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.sangjin_lee.typokeyboard;
 
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    int width;
+    int height;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView)findViewById(R.id.tv);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        width = size.x;
+        height = size.y;
+
+        tv.setText("x=>"+width+",y=>"+height);
     }
 }
